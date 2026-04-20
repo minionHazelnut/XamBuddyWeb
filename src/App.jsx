75c13f435b324eb6a95a2b373e7034b96d2e60fc
Minion <deeks@Users-MacBook-Pro.local>
2026-04-20 05:50:09 +0530

    Add student practice dashboard at /practice with MCQ, SA, LA, CBQ support

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AdminLogin from './pages/AdminLogin'
 import AdminDashboard from './pages/AdminDashboard'
+import StudentDashboard from './pages/StudentDashboard'
 import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<AdminLogin />} />
+        <Route path="/practice" element={<StudentDashboard />} />
         <Route
           path="/"
           element={
